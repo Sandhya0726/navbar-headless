908b4924a7edc374244789bee7008af58d6297a3
Sandhya <neupanesandhya4567@gamil.com>
2024-03-22 01:13:46 +0545

    <a> changed to <link>

--- src/Posts/Posts.jsx
+++ src/Posts/Posts.jsx
@@ -59,7 +59,7 @@ const Posts = () => {
               <Menu.Item>
                 {({ active }) => (
                   <Link
-                    href={item.href}
+                    to={item.href}
                     className={classNames(
                       active ? "bg-gray-100" : "",
                       "block px-4 py-2 text-sm text-gray-700"
